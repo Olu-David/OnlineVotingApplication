90ef64cfce911c441f1013ac5fdfdbadad324b01
STICKZZOFFICIAL <Olusanyadavid@yahoo.com>
2026-09-10 01:54:53 +0100

    ApplyAsCandidate(email)

--- OnlineVotingApplication/DataTransferView/CandidateApplicationViewModel.cs
+++ OnlineVotingApplication/DataTransferView/CandidateApplicationViewModel.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-public class CandidateApplicationViewModel
+namespace OnlineVotingApplication.DataTransferView
 {
-    public Guid? ElectionEventId { get; set; }
-    public Guid? TenantId { get; set; }
+    public class CandidateApplicationViewModel
+    {
+        public Guid? ElectionEventId { get; set; }
+        public Guid? TenantId { get; set; }
 
-    [Required]
-    [Display(Name = "First Name")]
-    public string FirstName { get; set; } = string.Empty;
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = string.Empty;
 
-    [Required]
-    [Display(Name = "Last Name")]
-    public string LastName { get; set; } = string.Empty;
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = string.Empty;
 
-    [Required, EmailAddress]
-    [Display(Name = "Email Address")]
-    public string Email { get; set; } = string.Empty;
+        [Required, EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; } = string.Empty;
 
-    [Required]
-    [Display(Name = "Select Position")]
-    public Guid? SelectedPositionId { get; set; }
+        [Required]
+        [Display(Name = "Select Position")]
+        public Guid? SelectedPositionId { get; set; }
 
-    public IEnumerable<SelectListItem> PositionOptions { get; set; } = new List<SelectListItem>();
+        public IEnumerable<SelectListItem> PositionOptions { get; set; } = new List<SelectListItem>();
+    }
 }
